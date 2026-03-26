0485605b426e82a56a8d60618e94730da2e6afc0
MatusBeke <matus.beke7@gmail.com>
2026-03-26 19:22:18 +0100

    n
    n

--- Menu/MenuScript.js
+++ Menu/MenuScript.js
@@ -1,4 +1,9 @@
 function startGame() {
-    document.getElementById('main-menu').style.display = 'none';
-    // Tu môžeš spustiť hudbu alebo inicializovať prvých štamgastov
+    // Skryje celé menu
+    document.getElementById('main-menu').style.opacity = '0';
+    setTimeout(() => {
+        document.getElementById('main-menu').style.display = 'none';
+        // Tu môžeš pridať spustenie hudby
+        console.log("Hra spustená...");
+    }, 500);
 }
