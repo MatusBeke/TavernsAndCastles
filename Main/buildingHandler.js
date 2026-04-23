337f14ddfe8ef767bd741cf720a41a02ca074e58
Jakub Tatar <jakub.tatar4@gmail.com>
2026-04-23 10:47:28 +0200

    Zaklad generovania npcs

--- Main/buildingHandler.js
+++ Main/buildingHandler.js
@@ -15,6 +15,41 @@ let currentFood = 1000000;
 
 let selectedTileForInfo = null;
 
+//NPCECKA
+var activeNPCs = [];
+
+class NPC{
+    constructor(id, name, profession, homeX, homeY, health, hunger, happiness) {
+        this.id = id;
+        this.name = name;
+        this.profession = profession;
+        this.homeX = homeX;
+        this.homeY = homeY;
+        this.health = health;
+        this.hunger = hunger;
+        this.happiness = happiness;
+    }
+
+    identify() {
+        console.log(`NPC ID: ${this.id}, Name: ${this.name}, Profession: ${this.profession}, Home: (${this.homeX}, ${this.homeY})`);
+    }
+}
+
+//TODO: Dokoncit generovanie NPCS
+function createNPC(homeX, homeY) {
+    var npc = new NPC(
+        activeNPCs.length + 1, 
+        `NPC${activeNPCs.length + 1}`, 
+        "Villager", 
+        homeX, 
+        homeY, 
+        100, 
+        100, 
+        100
+    );
+    activeNPCs.push(npc);
+}
+
 function toggleStats(menuId) {
     const menu = document.getElementById(menuId);
     menu.style.display = (menu.style.display === "none") ? "flex" : "none";
@@ -159,6 +194,16 @@ document.getElementById('gameCanvas').addEventListener('click', (e) => {
             
             currentGold -= currentBuildingPrice;
             updateHUD();
+
+            //Generovanie NPC pri postaveni budovy
+            console.log(`Built ${selectedBuildingSrc} at (${gridX}, ${gridY})`);
+            createNPC(gridX, gridY);
+            currentPop += activeNPCs.length;
+            updateHUD();
+            
+            activeNPCs.forEach(element => {
+                console.log(`NPC ${element.name} lives at (${element.homeX}, ${element.homeY}) with profession ${element.profession} and health ${element.health}`);
+            });
             finalizeBuild(canvas);
         }
     }
