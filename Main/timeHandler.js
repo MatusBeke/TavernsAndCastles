990e6b4fae2f733421f86b99f3a3a7b37da40425
Matúš Beke <bekem24@stud.spsknm.sk>
2026-04-23 10:00:27 +0200

    h
    h

---
+++ Main/timeHandler.js
@@ -0,0 +1,32 @@
+let currentDay = 1;
+let currentHour = 8; 
+let currentMinute = 0;
+let gameSpeed = 1000; 
+
+function updateTime() {
+    currentMinute += 10; 
+
+    if (currentMinute >= 60) {
+        currentMinute = 0;
+        currentHour++;
+    }
+
+    if (currentHour >= 24) {
+        currentHour = 0;
+        currentDay++;
+        
+    }
+
+    let formattedHour = currentHour < 10 ? "0" + currentHour : currentHour;
+    let formattedMinute = currentMinute < 10 ? "0" + currentMinute : currentMinute;
+
+    const dayEl = document.getElementById('day-text');
+    const timeEl = document.getElementById('time-text');
+    
+    if (dayEl && timeEl) {
+        dayEl.innerText = `Day ${currentDay}`;
+        timeEl.innerText = `${formattedHour}:${formattedMinute}`;
+    }
+}
+
+setInterval(updateTime, gameSpeed);
